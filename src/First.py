612caf558a2865f08a5d183c79a173684b6ac59a
TomCatLYZ <1073435404@qq.com>
2018-03-26 02:09:35 +0800

    2018.3.26修改

--- src/First.py
+++ src/First.py
@@ -12,3 +12,35 @@ for tag, *args in records:
         do_foo(*args)
     elif tag == 'bar':
         do_bar(*args)
+
+
+class Countdown:
+    def __init__(self, start):
+        self.start = start
+    # Forward iterator
+    def __iter__(self):
+        n = self.start
+        while n > 0:
+            yield n
+            n -= 1
+# Reverse iterator
+    def __reversed__(self):
+        n = 1
+        while n <= self.start:
+            yield n
+            n += 1
+for rr in reversed(Countdown(30)):
+    print(rr)
+for rr in Countdown(30):
+    print(rr)
+print("........................")
+def mininum(*values, clip=None):
+    m = min(values)
+    if clip is not None:
+        m = clip if clip > m else m
+    return m
+
+
+
+print(mininum(1, 5, 2, -5, 10))
+print(mininum(1, 5, 2, -5, 10, clip=0))
